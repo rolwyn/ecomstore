9dadc839c3268b977779e42b46ded24e947610f2
rolwyn <rolwyn.quadras051@gmail.com>
2025-03-09 00:05:42 +0530

    building ecom store

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: 'Home Page',
+}
+
+const HomePage = () => {
+  return <>ecom Store</>;
+}
+ 
+export default HomePage;
---
+++ lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Ecomstore';
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Ecommerce Store built with next.js';
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
