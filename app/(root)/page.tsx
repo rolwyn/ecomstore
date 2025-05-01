d3ce11544f4dd6780b6dace63988b28f87a4d99b
rolwyn <rolwyn.quadras051@gmail.com>
2025-05-01 19:07:41 +0530

    menu changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
+const delay = (ms: number | undefined) => new Promise((resolve) => setTimeout(resolve, ms))
+
 export const metadata = {
   title: 'Home Page',
 }
 
-const HomePage = () => {
-  return <>ecom Store</>;
+const HomePage = async () => {
+  await delay(2000)
+  return <>ecom Store</>
 }
  
 export default HomePage;
